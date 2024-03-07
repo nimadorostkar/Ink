fddaa05683339e0e42d0e8d673fd642da7fc03da
Fan Wang <nimadorostkar97@gmail.com>
2024-03-08 00:16:22 +0330

    admin

--- design/admin.py
+++ design/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from design.models import Category, Design
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title',]
+admin.site.register(Category, CategoryAdmin)
+
+
+
+class DesignAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'portfolio_homepage']
+    list_filter = ['portfolio_homepage',]
+admin.site.register(Design, DesignAdmin)
