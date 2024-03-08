50837054f6fde8ccf4d5d7d89c5597080eee3449
Fan Wang <nimadorostkar97@gmail.com>
2024-03-08 15:21:47 +0330

    portfolio

--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from homepage.models import HomepageInfo
 from contact.models import ContactMessage
+from design.models import Design
 
 
 def index(request):
     data = HomepageInfo.objects.all().first()
+    portfolio = Design.objects.filter(portfolio_homepage=True).order_by('-id')[:10]
     if request.method == 'POST':
         msg = ContactMessage()
         msg.name = request.POST['name']
         msg.email = request.POST['email']
         msg.message = request.POST['message']
         msg.save()
-    context = {'data':data}
+    context = {'data':data,'portfolio':portfolio}
     return render(request, 'index.html', context)
